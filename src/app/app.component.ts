5e42eed726de9ab5533031d254aa022263419330
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2024-02-23 18:06:50 -0300

    Implementação da estutura inicial do componente card.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Aula de Services</h1>
+    <app-card></app-card>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'services';
